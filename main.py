f95b4ebf3e1316082d5d8ad93641a98dab33f65c
ericstober <erictstober@gmail.com>
2025-12-28 22:09:43 -0600

    add get_char_counts which returns a dictionary of characters and number of times they appear given a text string

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_char_counts
 
 def get_book_text(filepath):
   with open(filepath) as f:
@@ -8,7 +8,10 @@ def get_book_text(filepath):
 
 def main():
   # print(get_book_text("./books/frankenstein.txt"))
-  num_words = get_num_words(get_book_text("./books/frankenstein.txt"))
+  book_text = get_book_text("./books/frankenstein.txt")
+  num_words = get_num_words(book_text)
+  char_counts = get_char_counts(book_text)
   print(f"Found {num_words} total words")
+  print(char_counts)
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,13 @@
 def get_num_words(text):
   return len(text.split())
+
+def get_char_counts(text):
+  char_counts = {}
+
+  for char in text.lower():
+    if char in char_counts:
+      char_counts[char] += 1
+    else:
+      char_counts[char] = 1
+
+  return char_counts
