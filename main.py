791604cc1495f5e0fcd609dad81447ad7b8aa552
ericstober <erictstober@gmail.com>
2025-12-28 22:58:15 -0600

    update program to take path instead of hardcoded value

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_char_counts, sort_char_counts
 
 def get_book_text(filepath):
@@ -5,15 +6,20 @@ def get_book_text(filepath):
     return f.read()
 
 def main():
-  book_path = "books/frankenstein.txt"
-  text = get_book_text(book_path)
+  # book_path = "books/frankenstein.txt"
+
+  if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+  text = get_book_text(sys.argv[1])
 
   word_count = get_num_words(text)
   char_counts = get_char_counts(text)
   sorted_chars = sort_char_counts(char_counts)
 
   print("============ BOOKBOT ============")
-  print(f"Analyzing book found at {book_path}...")
+  print(f"Analyzing book found at {sys.argv[1]}...")
   print("----------- Word Count ----------")
   print(f"Found {word_count} total words")
   print("--------- Character Count -------")
