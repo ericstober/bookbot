2e37e7bb12aafedece35cf7eb6218adf12b7a9fa
ericstober <erictstober@gmail.com>
2025-12-28 22:15:39 -0600

    simplify get_book_text

--- main.py
+++ main.py
@@ -2,12 +2,9 @@ from stats import get_num_words, get_char_counts
 
 def get_book_text(filepath):
   with open(filepath) as f:
-    file_contents = f.read()
-
-  return file_contents
+    return f.read()
 
 def main():
-  # print(get_book_text("./books/frankenstein.txt"))
   book_text = get_book_text("./books/frankenstein.txt")
   num_words = get_num_words(book_text)
   char_counts = get_char_counts(book_text)
