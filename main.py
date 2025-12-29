43491496ac226d4ae0ed381371f185bbb1fc27e1
ericstober <erictstober@gmail.com>
2025-12-28 21:36:03 -0600

    completed ch2 l5

---
+++ main.py
@@ -0,0 +1,14 @@
+from stats import get_num_words
+
+def get_book_text(filepath):
+  with open(filepath) as f:
+    file_contents = f.read()
+
+  return file_contents
+
+def main():
+  # print(get_book_text("./books/frankenstein.txt"))
+  num_words = get_num_words(get_book_text("./books/frankenstein.txt"))
+  print(f"Found {num_words} total words")
+
+main()
---
+++ stats.py
@@ -0,0 +1,4 @@
+def get_num_words(text):
+  split_text = text.split()
+  num_words = len(split_text)
+  return num_words
